15bdaab1fcd6d1f89274e54ddee297448c396294
XINEXPORT <christinehoangt@gmail.com>
2026-07-25 21:28:24 -0500

    adding variable expression example to basics.ml

--- basics.ml
+++ basics.ml
@@ -1,6 +1,13 @@
 let y : int = 3110
 let x = 42
 let z = 1 + 2
+let result = 
+      let s = 5 in
+      s + (let s = 6 in s)
 
 let () = 
 	Printf.printf "%d\n" (x + y)
+
+
+let () = 
+	Printf.printf "%d\n" result
