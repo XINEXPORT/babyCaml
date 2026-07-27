848f0b08afd81b042296b3cc0448dd0578dec40b
XINEXPORT <christinehoangt@gmail.com>
2026-07-26 19:51:21 -0500

    adding fun1 fun2 avg add fac

--- basics.ml
+++ basics.ml
@@ -5,9 +5,29 @@ let result =
       let s = 5 in
       s + (let s = 6 in s)
 
-let () = 
-	Printf.printf "%d\n" (x + y)
+let fun1 x = x+1
 
+let fun2 x y = x-y
 
-let () = 
-	Printf.printf "%d\n" result
+let avg x y = (x+.y) /. 2.
+
+let add x y = x + y
+
+(*requires : [n >=0] *)
+let rec fac n = 
+       if n = 0 then 1
+       else n * fac (n-1)
+
+let () = Printf.printf "%d\n" (x + y)
+
+let () = Printf.printf "%d\n" result
+
+let () = Printf.printf "%f\n" (avg 4. 8.)
+
+let () = Printf.printf "%d\n" (fun1(2+3))
+
+let () = Printf.printf "%d\n" (fun2 3 2)
+
+let () = Printf.printf "%d\n" (fac 5)
+
+let () = Printf.printf "%d\n" (add 2 3)
