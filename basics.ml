ab5bdfa94a80a13fb0eab5b6ba58a98a79e8267b
XINEXPORT <christinehoangt@gmail.com>
2026-07-28 20:03:32 -0500

    adding polymoprhic functions thru lists examples

--- basics.ml
+++ basics.ml
@@ -18,6 +18,20 @@ let rec fac n =
        if n = 0 then 1
        else n * fac (n-1)
 
+let id x=x
+
+let add_100 = (+) 100
+
+let mult_2 = ( * ) 2
+
+let eq = (=) 1
+
+let ( <^> ) x y = max x y
+
+let sqr x = x*x
+
+let lst = [[1; 3]; [5; 7]; [2; 32]]
+
 let () = Printf.printf "%d\n" (x + y)
 
 let () = Printf.printf "%d\n" result
@@ -31,3 +45,28 @@ let () = Printf.printf "%d\n" (fun2 3 2)
 let () = Printf.printf "%d\n" (fac 5)
 
 let () = Printf.printf "%d\n" (add 2 3)
+
+let () = Printf.printf "%d\n" (id 100)
+
+let () = Printf.printf "%d\n" ((+) 100 100)
+
+let () = Printf.printf "%d\n" (add_100 300)
+
+let () = Printf.printf "%d\n" (( * ) 2 2)
+
+let () = Printf.printf "%d\n" (mult_2 6)
+
+let () = Printf.printf "%b\n" (eq 2)
+
+let () = Printf.printf "%d\n" (max 0 1)
+
+let () = Printf.printf "%b\n" ((x <^> y) = max x y)
+
+let () = Printf.printf "%d\n" (succ 5)
+
+let () = Printf.printf "%d\n" (sqr(sqr (succ 5)))
+
+let () = Printf.printf "%d\n" (succ(sqr(sqr(succ 5))))
+
+let () = Printf.printf "%d\n" (5|>succ|>sqr|>sqr|>succ)
+
